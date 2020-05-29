08ccc1c07fd5f6c597bced7bc9947befbbe047b7
Jonathan Caley <jonathan.caley@ocado.com>
2020-05-29 16:01:23 +0100

    Delete irrelevant file

--- app/src/main/java/com/example/daggerexperimentation/EditTextUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.example.daggerexperimentation
-
-object EditTextUtils {
-    fun areDetailsAcceptable(username: String, password: String): Boolean {
-        return ((username.length > 4) && (password.length > 4))
-    }
-}
--- app/src/main/java/com/example/daggerexperimentation/ui/registration/termsandconditions/TermsAndConditionsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.example.daggerexperimentation.ui.registration.termsandconditions
-
-import androidx.lifecycle.ViewModel
-
-class TermsAndConditionsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
