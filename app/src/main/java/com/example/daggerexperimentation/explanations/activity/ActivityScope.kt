6b4b310831785e18b8b4a8ad6986015f6c28152b
Jonathan Caley <jonathan.caley@ocado.com>
2020-06-04 19:36:54 +0100

    Explanations of scope annotations

--- app/src/main/java/com/example/daggerexperimentation/explanations/activity/ActivityScope.kt
+++ app/src/main/java/com/example/daggerexperimentation/explanations/activity/ActivityScope.kt
@@ -8,6 +8,16 @@ import javax.inject.Scope
  * be defined
  */
 @Scope
+/**
+ * [MustBeDocumented] Informs tools that this should be part of
+ * the JavaDocs
+ */
 @MustBeDocumented
+/**
+ * [Retention] Informs the compiler the level of persistence of
+ * the annotation during the build execution. Runtime means the
+ * annotation persists up until the exucution of the code you
+ * annotated
+ */
 @Retention(value = AnnotationRetention.RUNTIME)
 annotation class ActivityScope
